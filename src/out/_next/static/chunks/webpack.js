50aee30a530d640cc893ad04ad77f2259d828e49
Francis Luz <francisluz@gmail.com>
2021-08-04 12:54:47 +0100

    Repo clean up removing old folder structure

--- src/out/_next/static/chunks/webpack.js
+++ src/out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c311b3e92981009dddd2";
+/******/ 	var hotCurrentHash = "0d287a65af5db20be13b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
